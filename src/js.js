b58880f70a59531cad02f91df0e4445b9eb4cf47
Kris Sawyerr <krisawyerr@gmail.com>
2023-09-20 17:21:39 -0700

    index in mian

--- src/js.js
+++ src/js.js
@@ -1,88 +1,88 @@
 
 document.getElementById("W1D1").addEventListener("click", function () {
-    document.getElementById("Vid").innerHTML = "<iframe width='1280' height='720' src='W1D1.mp4' frameborder='0' allow='accelerometer; clipboard-write; encrypted-media; gyroscope; web-share' allowfullscreen=''></iframe>";
+    document.getElementById("Vid").innerHTML = "<iframe width='1280' height='720' src='/src/W1D1.mp4' frameborder='0' allow='accelerometer; clipboard-write; encrypted-media; gyroscope; web-share' allowfullscreen=''></iframe>";
 });
 document.getElementById("W1D2").addEventListener("click", function () {
-    document.getElementById("Vid").innerHTML = "<iframe width='1280' height='720' src='W1D2.mp4' frameborder='0' allow='accelerometer; clipboard-write; encrypted-media; gyroscope; web-share' allowfullscreen=''></iframe>";
+    document.getElementById("Vid").innerHTML = "<iframe width='1280' height='720' src='/src/W1D2.mp4' frameborder='0' allow='accelerometer; clipboard-write; encrypted-media; gyroscope; web-share' allowfullscreen=''></iframe>";
 });
 document.getElementById("W1D3").addEventListener("click", function () {
-    document.getElementById("Vid").innerHTML = "<iframe width='1280' height='720' src='W1D3.mp4' frameborder='0' allow='accelerometer; clipboard-write; encrypted-media; gyroscope; web-share' allowfullscreen=''></iframe>";
+    document.getElementById("Vid").innerHTML = "<iframe width='1280' height='720' src='/src/W1D3.mp4' frameborder='0' allow='accelerometer; clipboard-write; encrypted-media; gyroscope; web-share' allowfullscreen=''></iframe>";
 });
 document.getElementById("W1D4").addEventListener("click", function () {
-    document.getElementById("Vid").innerHTML = "<iframe width='1280' height='720' src='W1D4.mp4' frameborder='0' allow='accelerometer; clipboard-write; encrypted-media; gyroscope; web-share' allowfullscreen=''></iframe>";
+    document.getElementById("Vid").innerHTML = "<iframe width='1280' height='720' src='/src/W1D4.mp4' frameborder='0' allow='accelerometer; clipboard-write; encrypted-media; gyroscope; web-share' allowfullscreen=''></iframe>";
 });
 document.getElementById("W1D5").addEventListener("click", function () {
-    document.getElementById("Vid").innerHTML = "<iframe width='1280' height='720' src='W1D5.mp4' frameborder='0' allow='accelerometer; clipboard-write; encrypted-media; gyroscope; web-share' allowfullscreen=''></iframe>";
+    document.getElementById("Vid").innerHTML = "<iframe width='1280' height='720' src='/src/W1D5.mp4' frameborder='0' allow='accelerometer; clipboard-write; encrypted-media; gyroscope; web-share' allowfullscreen=''></iframe>";
 });
 document.getElementById("W1D6").addEventListener("click", function () {
-    document.getElementById("Vid").innerHTML = "<iframe width='1280' height='720' src='W1D6.mp4' frameborder='0' allow='accelerometer; clipboard-write; encrypted-media; gyroscope; web-share' allowfullscreen=''></iframe>";
+    document.getElementById("Vid").innerHTML = "<iframe width='1280' height='720' src='/src/W1D6.mp4' frameborder='0' allow='accelerometer; clipboard-write; encrypted-media; gyroscope; web-share' allowfullscreen=''></iframe>";
 });
 document.getElementById("W1D7").addEventListener("click", function () {
-    document.getElementById("Vid").innerHTML = "<iframe width='1280' height='720' src='W1D7.mp4' frameborder='0' allow='accelerometer; clipboard-write; encrypted-media; gyroscope; web-share' allowfullscreen=''></iframe>";
+    document.getElementById("Vid").innerHTML = "<iframe width='1280' height='720' src='/src/W1D7.mp4' frameborder='0' allow='accelerometer; clipboard-write; encrypted-media; gyroscope; web-share' allowfullscreen=''></iframe>";
 });
 
 document.getElementById("W2D1").addEventListener("click", function () {
-    document.getElementById("Vid").innerHTML = "<iframe width='1280' height='720' src='W2D1.mp4' frameborder='0' allow='accelerometer; clipboard-write; encrypted-media; gyroscope; web-share' allowfullscreen=''></iframe>";
+    document.getElementById("Vid").innerHTML = "<iframe width='1280' height='720' src='/src/W2D1.mp4' frameborder='0' allow='accelerometer; clipboard-write; encrypted-media; gyroscope; web-share' allowfullscreen=''></iframe>";
 });
 document.getElementById("W2D2").addEventListener("click", function () {
-    document.getElementById("Vid").innerHTML = "<iframe width='1280' height='720' src='W2D2.mp4' frameborder='0' allow='accelerometer; clipboard-write; encrypted-media; gyroscope; web-share' allowfullscreen=''></iframe>";
+    document.getElementById("Vid").innerHTML = "<iframe width='1280' height='720' src='/src/W2D2.mp4' frameborder='0' allow='accelerometer; clipboard-write; encrypted-media; gyroscope; web-share' allowfullscreen=''></iframe>";
 });
 document.getElementById("W2D3").addEventListener("click", function () {
-    document.getElementById("Vid").innerHTML = "<iframe width='1280' height='720' src='W2D3.mp4' frameborder='0' allow='accelerometer; clipboard-write; encrypted-media; gyroscope; web-share' allowfullscreen=''></iframe>";
+    document.getElementById("Vid").innerHTML = "<iframe width='1280' height='720' src='/src/W2D3.mp4' frameborder='0' allow='accelerometer; clipboard-write; encrypted-media; gyroscope; web-share' allowfullscreen=''></iframe>";
 });
 document.getElementById("W2D4").addEventListener("click", function () {
-    document.getElementById("Vid").innerHTML = "<iframe width='1280' height='720' src='W2D4.mp4' frameborder='0' allow='accelerometer; clipboard-write; encrypted-media; gyroscope; web-share' allowfullscreen=''></iframe>";
+    document.getElementById("Vid").innerHTML = "<iframe width='1280' height='720' src='/src/W2D4.mp4' frameborder='0' allow='accelerometer; clipboard-write; encrypted-media; gyroscope; web-share' allowfullscreen=''></iframe>";
 });
 document.getElementById("W2D5").addEventListener("click", function () {
-    document.getElementById("Vid").innerHTML = "<iframe width='1280' height='720' src='W2D5.mp4' frameborder='0' allow='accelerometer; clipboard-write; encrypted-media; gyroscope; web-share' allowfullscreen=''></iframe>";
+    document.getElementById("Vid").innerHTML = "<iframe width='1280' height='720' src='/src/W2D5.mp4' frameborder='0' allow='accelerometer; clipboard-write; encrypted-media; gyroscope; web-share' allowfullscreen=''></iframe>";
 });
 document.getElementById("W2D6").addEventListener("click", function () {
-    document.getElementById("Vid").innerHTML = "<iframe width='1280' height='720' src='W2D6.mp4' frameborder='0' allow='accelerometer; clipboard-write; encrypted-media; gyroscope; web-share' allowfullscreen=''></iframe>";
+    document.getElementById("Vid").innerHTML = "<iframe width='1280' height='720' src='/src/W2D6.mp4' frameborder='0' allow='accelerometer; clipboard-write; encrypted-media; gyroscope; web-share' allowfullscreen=''></iframe>";
 });
 document.getElementById("W2D7").addEventListener("click", function () {
-    document.getElementById("Vid").innerHTML = "<iframe width='1280' height='720' src='W2D7.mp4' frameborder='0' allow='accelerometer; clipboard-write; encrypted-media; gyroscope; web-share' allowfullscreen=''></iframe>";
+    document.getElementById("Vid").innerHTML = "<iframe width='1280' height='720' src='/src/W2D7.mp4' frameborder='0' allow='accelerometer; clipboard-write; encrypted-media; gyroscope; web-share' allowfullscreen=''></iframe>";
 });
 
 document.getElementById("W3D1").addEventListener("click", function () {
-    document.getElementById("Vid").innerHTML = "<iframe width='1280' height='720' src='W3D1.mp4' frameborder='0' allow='accelerometer; clipboard-write; encrypted-media; gyroscope; web-share' allowfullscreen=''></iframe>";
+    document.getElementById("Vid").innerHTML = "<iframe width='1280' height='720' src='/src/W3D1.mp4' frameborder='0' allow='accelerometer; clipboard-write; encrypted-media; gyroscope; web-share' allowfullscreen=''></iframe>";
 });
 document.getElementById("W3D2").addEventListener("click", function () {
-    document.getElementById("Vid").innerHTML = "<iframe width='1280' height='720' src='W3D2.mp4' frameborder='0' allow='accelerometer; clipboard-write; encrypted-media; gyroscope; web-share' allowfullscreen=''></iframe>";
+    document.getElementById("Vid").innerHTML = "<iframe width='1280' height='720' src='/src/W3D2.mp4' frameborder='0' allow='accelerometer; clipboard-write; encrypted-media; gyroscope; web-share' allowfullscreen=''></iframe>";
 });
 document.getElementById("W3D3").addEventListener("click", function () {
-    document.getElementById("Vid").innerHTML = "<iframe width='1280' height='720' src='W3D3.mp4' frameborder='0' allow='accelerometer; clipboard-write; encrypted-media; gyroscope; web-share' allowfullscreen=''></iframe>";
+    document.getElementById("Vid").innerHTML = "<iframe width='1280' height='720' src='/src/W3D3.mp4' frameborder='0' allow='accelerometer; clipboard-write; encrypted-media; gyroscope; web-share' allowfullscreen=''></iframe>";
 });
 document.getElementById("W3D4").addEventListener("click", function () {
-    document.getElementById("Vid").innerHTML = "<iframe width='1280' height='720' src='W3D4.mp4' frameborder='0' allow='accelerometer; clipboard-write; encrypted-media; gyroscope; web-share' allowfullscreen=''></iframe>";
+    document.getElementById("Vid").innerHTML = "<iframe width='1280' height='720' src='/src/W3D4.mp4' frameborder='0' allow='accelerometer; clipboard-write; encrypted-media; gyroscope; web-share' allowfullscreen=''></iframe>";
 });
 document.getElementById("W3D5").addEventListener("click", function () {
-    document.getElementById("Vid").innerHTML = "<iframe width='1280' height='720' src='W3D5.mp4' frameborder='0' allow='accelerometer; clipboard-write; encrypted-media; gyroscope; web-share' allowfullscreen=''></iframe>";
+    document.getElementById("Vid").innerHTML = "<iframe width='1280' height='720' src='/src/W3D5.mp4' frameborder='0' allow='accelerometer; clipboard-write; encrypted-media; gyroscope; web-share' allowfullscreen=''></iframe>";
 });
 document.getElementById("W3D6").addEventListener("click", function () {
-    document.getElementById("Vid").innerHTML = "<iframe width='1280' height='720' src='W3D6.mp4' frameborder='0' allow='accelerometer; clipboard-write; encrypted-media; gyroscope; web-share' allowfullscreen=''></iframe>";
+    document.getElementById("Vid").innerHTML = "<iframe width='1280' height='720' src='/src/W3D6.mp4' frameborder='0' allow='accelerometer; clipboard-write; encrypted-media; gyroscope; web-share' allowfullscreen=''></iframe>";
 });
 document.getElementById("W3D7").addEventListener("click", function () {
-    document.getElementById("Vid").innerHTML = "<iframe width='1280' height='720' src='W3D7.mp4' frameborder='0' allow='accelerometer; clipboard-write; encrypted-media; gyroscope; web-share' allowfullscreen=''></iframe>";
+    document.getElementById("Vid").innerHTML = "<iframe width='1280' height='720' src='/src/W3D7.mp4' frameborder='0' allow='accelerometer; clipboard-write; encrypted-media; gyroscope; web-share' allowfullscreen=''></iframe>";
 });
 
 document.getElementById("W4D1").addEventListener("click", function () {
-    document.getElementById("Vid").innerHTML = "<iframe width='1280' height='720' src='W4D1.mp4' frameborder='0' allow='accelerometer; clipboard-write; encrypted-media; gyroscope; web-share' allowfullscreen=''></iframe>";
+    document.getElementById("Vid").innerHTML = "<iframe width='1280' height='720' src='/src/W4D1.mp4' frameborder='0' allow='accelerometer; clipboard-write; encrypted-media; gyroscope; web-share' allowfullscreen=''></iframe>";
 });
 document.getElementById("W4D2").addEventListener("click", function () {
-    document.getElementById("Vid").innerHTML = "<iframe width='1280' height='720' src='W4D2.mp4' frameborder='0' allow='accelerometer; clipboard-write; encrypted-media; gyroscope; web-share' allowfullscreen=''></iframe>";
+    document.getElementById("Vid").innerHTML = "<iframe width='1280' height='720' src='/src/W4D2.mp4' frameborder='0' allow='accelerometer; clipboard-write; encrypted-media; gyroscope; web-share' allowfullscreen=''></iframe>";
 });
 document.getElementById("W4D3").addEventListener("click", function () {
-    document.getElementById("Vid").innerHTML = "<iframe width='1280' height='720' src='W4D3.mp4' frameborder='0' allow='accelerometer; clipboard-write; encrypted-media; gyroscope; web-share' allowfullscreen=''></iframe>";
+    document.getElementById("Vid").innerHTML = "<iframe width='1280' height='720' src='/src/W4D3.mp4' frameborder='0' allow='accelerometer; clipboard-write; encrypted-media; gyroscope; web-share' allowfullscreen=''></iframe>";
 });
 document.getElementById("W4D4").addEventListener("click", function () {
-    document.getElementById("Vid").innerHTML = "<iframe width='1280' height='720' src='W4D4.mp4' frameborder='0' allow='accelerometer; clipboard-write; encrypted-media; gyroscope; web-share' allowfullscreen=''></iframe>";
+    document.getElementById("Vid").innerHTML = "<iframe width='1280' height='720' src='/src/W4D4.mp4' frameborder='0' allow='accelerometer; clipboard-write; encrypted-media; gyroscope; web-share' allowfullscreen=''></iframe>";
 });
 document.getElementById("W4D5").addEventListener("click", function () {
-    document.getElementById("Vid").innerHTML = "<iframe width='1280' height='720' src='W4D5.mp4' frameborder='0' allow='accelerometer; clipboard-write; encrypted-media; gyroscope; web-share' allowfullscreen=''></iframe>";
+    document.getElementById("Vid").innerHTML = "<iframe width='1280' height='720' src='/src/W4D5.mp4' frameborder='0' allow='accelerometer; clipboard-write; encrypted-media; gyroscope; web-share' allowfullscreen=''></iframe>";
 });
 document.getElementById("W4D6").addEventListener("click", function () {
-    document.getElementById("Vid").innerHTML = "<iframe width='1280' height='720' src='W4D6.mp4' frameborder='0' allow='accelerometer; clipboard-write; encrypted-media; gyroscope; web-share' allowfullscreen=''></iframe>";
+    document.getElementById("Vid").innerHTML = "<iframe width='1280' height='720' src='/src/W4D6.mp4' frameborder='0' allow='accelerometer; clipboard-write; encrypted-media; gyroscope; web-share' allowfullscreen=''></iframe>";
 });
 document.getElementById("W4D7").addEventListener("click", function () {
-    document.getElementById("Vid").innerHTML = "<iframe width='1280' height='720' src='W4D7.mp4' frameborder='0' allow='accelerometer; clipboard-write; encrypted-media; gyroscope; web-share' allowfullscreen=''></iframe>";
+    document.getElementById("Vid").innerHTML = "<iframe width='1280' height='720' src='/src/W4D7.mp4' frameborder='0' allow='accelerometer; clipboard-write; encrypted-media; gyroscope; web-share' allowfullscreen=''></iframe>";
 });
